84407ff96e231859330c3caf5deeea4ec66c6fe0
Jonathan Riquer <jonriquer@gmail.com>
2019-06-23 21:48:16 -0400

    initializing passport setup

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://jonriquer:mclarenf1@cluster0-cetbg.mongodb.net/budgetApp?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://jonriquer:mclarenf1@cluster0-cetbg.mongodb.net/budgetApp?retryWrites=true&w=majority',
+  secretOrKey: "secret"
 }
