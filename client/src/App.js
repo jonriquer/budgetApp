e6a64c8628a552871dc906cb97187e3f0f809c3e
Jonathan Riquer <jonriquer@gmail.com>
2019-06-28 17:49:59 -0400

    creating auth dir && creating/adding initial code to Login.js && Register.js, also importing Login.js && Register.js && Route, also adding Routes for Login.js && Register.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
@@ -10,7 +12,9 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-          <Landing />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
         </div>
       </Router>
     );
